7e9d1da0e4b904af5a00c2ee10040ca1e38d4ab1
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2019-10-20 13:22:37 +0300

    add UI

---
+++ Parking.Web/wwwroot/js/JavaScript.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        url: "/api/Cars",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            $("#DIV").html('');
+            var DIV = '';
+            $.each(data, function (i, item) {
+                var rows = "<tr>" +
+                    "<td id='Id'>" + item.id + "</td>" +
+                    "<td id='Brand'>" + item.brand + "</td>" +
+                    "<td id='Car plate'>" + item.carPlate + "</td>" +
+                    "</tr>";
+                $('#Table').append(rows);
+            });
+            console.log(data);
+        },
+
+        failure: function (data) {
+            alert(data.responseText);
+        },
+        error: function (data) {
+            alert(data.responseText);
+        }
+
+    });
+});
