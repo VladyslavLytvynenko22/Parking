e3aa0b5946878d0a05547ec3c13cb22da98319e1
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2019-10-25 13:37:45 +0300

    Revert random commit add DTO

--- Parking.Domain/Models/Garage.cs
+++ Parking.Domain/Models/Garage.cs
@@ -1,11 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
 namespace Parking.Domain.Models
 {
     public class Garage
     {
         public int Id { get; set; }
-        [Required]
         public double Area { get; set; }
         public string Color { get; set; }
         public int? CarId { get; set; }
