cfd046772f1e1102e837a07caf613adfd0c8752f
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2019-10-25 13:36:55 +0300

    Revert random commit add DTO

--- Parking.Domain/Models/Car.cs
+++ Parking.Domain/Models/Car.cs
@@ -1,16 +1,12 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace Parking.Domain.Models
 {
     public class Car
     {
         public int Id { get; set; }
-        [Required]
         public string Brand { get; set; }
-        [Required]
         public string CarPlate { get; set; }
-        [Required]
         public int OwnerId { get; set; }
         public string Photo { get; set; }
 
