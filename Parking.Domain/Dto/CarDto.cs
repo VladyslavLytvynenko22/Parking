edbf781fcb8b7b404a78cb474846c1be2a51caf1
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2019-10-25 15:44:15 +0300

    Revert "Revert random commit add DTO"
    This reverts commit b62c3d3d68e63cb50256a49bd23d2a51d3126221.

---
+++ Parking.Domain/Dto/CarDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parking.Domain.Dto
+{
+    public class CarDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Brand { get; set; }
+        [Required]
+        public string CarPlate { get; set; }
+        [Required]
+        public int OwnerId { get; set; }
+        public string Photo { get; set; }
+    }
+}
