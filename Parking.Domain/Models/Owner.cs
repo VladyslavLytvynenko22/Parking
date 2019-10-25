fc02428c65a2019092c277dba9dc991b180378b9
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2019-10-25 15:43:56 +0300

    Revert "Revert random commit add DTO"
    This reverts commit 50c6058714fe646ca0bf36adf1c76330ad2718dd.

--- Parking.Domain/Models/Owner.cs
+++ Parking.Domain/Models/Owner.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Parking.Domain.Models
 {
     public class Owner
     {
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
-        public DateTime? Dob { get; set; }
+        public DateTime? DateOfBirth { get; set; }
 
         public List<Car> Cars { get; set; } = new List<Car>();
     }
