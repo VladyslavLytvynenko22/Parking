9083e0eb357598f9c8a87b6d95159efd80c6172e
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2019-11-15 16:25:34 +0200

    clearing code

--- Parking.Web/Startup.cs
+++ Parking.Web/Startup.cs
@@ -30,21 +30,12 @@ namespace Parking.Web
                         options.RequireHttpsMetadata = false;
                         options.TokenValidationParameters = new TokenValidationParameters
                         {
-                            // укзывает, будет ли валидироваться издатель при валидации токена
                             ValidateIssuer = true,
-                            // строка, представляющая издателя
                             ValidIssuer = AuthOptions.ISSUER,
-
-                            // будет ли валидироваться потребитель токена
                             ValidateAudience = true,
-                            // установка потребителя токена
                             ValidAudience = AuthOptions.AUDIENCE,
-                            // будет ли валидироваться время существования
                             ValidateLifetime = true,
-
-                            // установка ключа безопасности
                             IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
-                            // валидация ключа безопасности
                             ValidateIssuerSigningKey = true,
                         };
                     });
